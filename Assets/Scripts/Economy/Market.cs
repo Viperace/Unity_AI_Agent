0af6e2978d74d990d57328d899857175444781c1
Viperace <Viperace@users.noreply.github.com>
2021-02-02 16:24:47 +0800

    Update to add GBM

--- Assets/Scripts/Economy/Market.cs
+++ Assets/Scripts/Economy/Market.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Superbest_random;
 
 public class Market : MonoBehaviour
 {
@@ -8,7 +9,11 @@ public class Market : MonoBehaviour
     public static Market Instance { get { return _instance; } }
 
     Dictionary<Commodity, float> prices = new Dictionary<Commodity, float>();
-
+	Dictionary<Commodity, float> volatilities = new Dictionary<Commodity, float>();
+    float _stepSize = 60; // in seconds
+    float _stepCooldown;
+	
+	
     void Awake()
     {
         if (_instance != null && _instance != this)
@@ -24,10 +29,14 @@ public class Market : MonoBehaviour
         prices.Add(Commodity.ORE, 5);
         prices.Add(Commodity.STEEL, 25);
         prices.Add(Commodity.ORICHALCUM, 500);
+		
+		// Init vol
+		volatilities.Add(Commodity.GOLD, 0);
+        volatilities.Add(Commodity.ORE, 0.3);
+        volatilities.Add(Commodity.STEEL, 0.15);
+        volatilities.Add(Commodity.ORICHALCUM, 0.2);		
     }
 
-    float _stepSize = 60; // in seconds
-    float _stepCooldown;
     void Update()
     {
         if(_stepCooldown < 0)
@@ -37,14 +46,32 @@ public class Market : MonoBehaviour
         }
         _stepCooldown -= Time.deltaTime;
     }
+	
     void SimulatePriceOneStep()
     {
-
+		/* dS = mu*dt + sig*dW
+			S(t)=S(t-1)*exp((mu-0.5*sig^2)*T + sig*sqrt(T)*Z)
+			where Z~N(0, 1)
+		*/
+		
+		// TODO: Move to start()?
+		var rng = new Random();
+		foreach(Commodity comm in Enum.GetValues(typeof(Commodity)))
+		{
+			//Commodity comm = Commodity.ORE;
+			float vol = volatilities[comm];
+			float S0 = prices[comm];
+			float dt = _stepSize / Mathf.Sqrt(250);
+			float eps = rng.NextGaussian();
+			float Snew = S0*Mathf.Exp(-0.5*vol*vol*dt + vol*Mathf.Sqrt(dt)*eps);
+			prices[comm] = Snew;
+		}
+		
     }
 
     public float GetValue(Commodity commodity)
     {
-        return prices[commodity];
+        return Mathf.RoundToInt(prices[commodity]);
     }
 }
 
