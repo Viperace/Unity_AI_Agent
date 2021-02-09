ea00694d787bfc83c4dc47e5863755e5f6b943e0
Viperace <voonhooi@gmail.com>
2021-02-09 09:06:22 +0800

    Hex game clean up

---
+++ Assets/HexagonGame/PolygonUIGame.cs
@@ -0,0 +1,192 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+using TMPro;
+public class PolygonUIGame : MonoBehaviour
+{
+    List<Button> buttons;
+    List<TMP_Text> labelTexts;
+    [SerializeField] UIPolygon targetHex;
+    [SerializeField] UIPolygon currentHex;
+    [SerializeField] UIPolygon backgroundHex;
+    [SerializeField] float triggerPercentage = 0.1f;
+    Polygon target;
+    Polygon current;
+    int maxValue = 6;
+    int minValue = 1;
+    int _currentNvertices;
+
+    public float score { get; private set; }
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (current != null && labelTexts != null)
+            for (int i = 0; i < labelTexts.Count; i++)
+            {
+                labelTexts[i].text = string.Concat(current.hexagonValues[i], "/6");
+            }
+    }
+
+    /// <summary>
+    /// This is to overcome the very odd bug. UIPolygon will NOT RESPECT set vertices at first load of new vertices. We need to wait for X frames or time, and reload it
+    /// Thereafter it will respect,
+    /// </summary>
+    public void NewGame(int N = 6)
+    {
+        StartCoroutine(NewGameUnfix(N));
+        StartCoroutine(NewGameUnfix(N, 5));
+
+        SpawnPolygonButtons spawnButtons = GetComponentInChildren<SpawnPolygonButtons>();
+        if (spawnButtons)
+            spawnButtons.SpawnButtonsForNewGame(N);
+    }
+    IEnumerator NewGameUnfix(int N, float nFrame=1)
+    {
+        _currentNvertices = N;
+        
+        // Randomize hex to solve        
+        target = new Polygon(N);
+        for (int i = 0; i < N; i++)
+            target.hexagonValues[i] = Random.Range(minValue, maxValue + 1);
+
+        // Randomize Initial
+        current = new Polygon(N);
+        for (int i = 0; i < N; i++)
+            current.hexagonValues[i] = Mathf.Min(Random.Range(1, 3), target.hexagonValues[i]);
+
+        // Set polygon size
+        targetHex.sides = N;
+        currentHex.sides = N;
+        backgroundHex.sides = N;
+
+        // Wait for N frames
+        for(int i = 0; i < nFrame; i++)
+            yield return null;
+
+        // Refresh
+        backgroundHex.SetVerticesDirty();
+        targetHex.SetVerticesDirty();
+        currentHex.SetVerticesDirty();
+        StartCoroutine(_ForcedUpdateUI(backgroundHex, Polygon.MaxValue(N, maxValue)));
+        StartCoroutine(_ForcedUpdateUI(targetHex, target));
+        StartCoroutine(_ForcedUpdateUI(currentHex, current));
+
+        //_TestdUpdateUI(backgroundHex, Polygon.MaxValue(N, maxValue));
+        //_TestdUpdateUI(targetHex, target);
+        //_TestdUpdateUI(currentHex, current);
+    }
+
+    public void _PushButton(int i)
+    {
+        if (current != null && current.hexagonValues.Length >= i)
+        {
+            current.hexagonValues[i]++;
+            current.hexagonValues[i] = Mathf.Min(maxValue, current.hexagonValues[i]);
+
+            if (Random.value < triggerPercentage)
+            {
+                int left = current.GetLeftNeighbor(i);
+                int right = current.GettRightNeighbor(i);
+
+                current.hexagonValues[left]++;
+                current.hexagonValues[left] = Mathf.Min(maxValue, current.hexagonValues[left]);
+
+                current.hexagonValues[right]++;
+                current.hexagonValues[right] = Mathf.Min(maxValue, current.hexagonValues[right]);
+            }
+        }
+
+        // Update score
+        this.score = GetScore();
+
+        // Update UI        
+        _TestdUpdateUI(currentHex, current);
+    }
+
+    void _TestdUpdateUI(UIPolygon hexUI, Polygon hexvalue)
+    {
+        hexUI.SetVerticesDirty();
+
+        // Update UI Value
+        if (hexvalue != null)
+            for (int i = 0; i < hexvalue.Nvertices; i++)
+                hexUI.VerticesDistances[i] = hexvalue.hexagonValues[i] / ((float)maxValue);
+    }
+
+    IEnumerator _ForcedUpdateUI(UIPolygon hexUI, Polygon hexvalue)
+    {
+        yield return null; // Must give time to let the vertices update
+
+        // Update UI Value
+        if (hexvalue != null)
+            for (int i = 0; i < hexvalue.Nvertices; i++)
+                hexUI.VerticesDistances[i] = hexvalue.hexagonValues[i] / ((float)maxValue);
+
+        yield return null;
+    }
+
+    // The lowest the better
+    float GetScore()
+    {
+        if (current != null)
+        {
+            int n = current.hexagonValues.Length;
+            float score = 0;
+            for (int i = 0; i < n; i++)
+                score += current.hexagonValues[i] - target.hexagonValues[i];
+
+            //score /= n;
+            return score;
+        }
+        else
+            return -1;
+    }
+}
+
+
+
+class Polygon
+{
+    // Start with topright
+    public int[] hexagonValues { get; private set; }
+    public int Nvertices { get; private set; }
+    public Polygon(int N) 
+    {
+        hexagonValues = new int[N];
+        for (int i = 0; i < hexagonValues.Length; i++)
+            hexagonValues[i] = 1;
+
+        Nvertices = N;
+    }
+
+    public int GetLeftNeighbor(int i)
+    {
+        int x = i - 1;
+        if (x < 0)
+            return hexagonValues.Length - 1;
+        return x;
+    }
+
+    public int GettRightNeighbor(int i)
+    {
+        int x = i + 1;
+        if (x == hexagonValues.Length)
+            return 0;
+        return x;
+    }
+
+    public static Polygon MaxValue(int N, int maxValue)
+    {
+        Polygon p = new Polygon(N);
+        for (int i = 0; i < p.Nvertices; i++)
+            p.hexagonValues[i] = maxValue;
+        return p;
+    }
+}
---
+++ Assets/HexagonGame/SpawnPolygonButtons.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+using TMPro;
+
+public class SpawnPolygonButtons : MonoBehaviour
+{
+    [SerializeField] UIPolygon backgroundPolygon;
+    [SerializeField] TMP_Text cornerTextPrefab;
+    [SerializeField] GameObject cornerButtonPrefab;
+    PolygonUIGame polygonGame;
+
+    [SerializeField] float _radiusOffset = 100;
+    [SerializeField] float _buttonYOffset = 50;
+    [SerializeField] float _textYOffset = -50;
+    float _radius;
+    Vector2[] _cornerLocations;
+    Button[] _buttons;
+    TMP_Text[] _texts;
+    void Start()
+    {
+        // Init
+        _radius = (float) 0.5*backgroundPolygon.rectTransform.rect.width;
+        polygonGame = GetComponentInParent<PolygonUIGame>();
+
+    }
+
+    Vector2[] FindCornerLocations(int N)
+    {
+        float angle = 360f / ((float)N) * Mathf.Deg2Rad;
+
+        List<Vector2> pos = new List<Vector2>();
+        for(int i = 0; i < N; i++)
+        {
+            float x = (_radius + _radiusOffset) * Mathf.Cos(angle * i);
+            float y = (_radius + _radiusOffset) * Mathf.Sin(angle * i);
+            pos.Add(new Vector2(x, y));
+        }
+
+        return pos.ToArray();
+    }
+
+    Button[] SpawnButtonsAroundCorners()
+    {
+        List<Button> buttons = new List<Button>();
+        for(int i = 0; i < _cornerLocations.Length; i++)
+        {
+            Vector2 pos = _cornerLocations[i];
+
+            // Create button and put into location
+            GameObject buttonGO = Instantiate(cornerButtonPrefab, this.transform);
+            buttonGO.GetComponent<RectTransform>().localPosition = pos + new Vector2(0, _buttonYOffset);
+
+            // Attach function
+            int x = i;
+            buttonGO.GetComponent<Button>().onClick.AddListener(() => polygonGame._PushButton(x));
+            buttons.Add(buttonGO.GetComponent<Button>());
+        }
+        return buttons.ToArray();
+    }
+
+    TMP_Text[] SpawnTextsAroundCorners()
+    {
+        List<TMP_Text> buttons = new List<TMP_Text>();
+        foreach (Vector2 pos in _cornerLocations)
+        {
+            TMP_Text t = Instantiate<TMP_Text>(cornerTextPrefab, this.transform);
+            t.GetComponent<RectTransform>().localPosition = pos + new Vector2(0, _textYOffset);
+            buttons.Add(t);
+        }
+        return buttons.ToArray();
+    }
+
+    TMP_Text[] SpawnTextsBelowButtons()
+    {
+        List<TMP_Text> texts = new List<TMP_Text>();
+        foreach (Button btn in _buttons)
+            if (btn != null)
+            {
+                TMP_Text t = Instantiate<TMP_Text>(cornerTextPrefab, btn.transform);
+                t.GetComponent<RectTransform>().localPosition = new Vector2(0, _textYOffset);
+                texts.Add(t);
+            }
+        return texts.ToArray();
+    }
+
+    public void SpawnButtonsForNewGame(int N)
+    {
+        // Remove old buttons
+        if(_texts != null)
+            foreach (TMP_Text t in _texts)
+                Destroy(t.gameObject);
+
+        if(_buttons != null)
+            foreach (Button btn in _buttons)
+                Destroy(btn.gameObject);
+
+        // Instantiate new one
+        _cornerLocations = FindCornerLocations(N);
+        _buttons = SpawnButtonsAroundCorners();
+        _texts = SpawnTextsBelowButtons();
+
+    }
+}
